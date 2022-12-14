23273626837d29a66d4887cd62777c80ced383a5
vforvilela <henrique@vilela.one>
2022-12-14 00:37:28 -0300

    feat: Include the new price providers as fallback

--- src/domain/ticker/ticker.service.spec.ts
+++ src/domain/ticker/ticker.service.spec.ts
@@ -1,14 +1,36 @@
 import { HttpModule } from '@nestjs/axios'
 import { Test, TestingModule } from '@nestjs/testing'
-import { BinanceRepository } from './repositories/binance.repository'
 import { TickerService } from './ticker.service'
+
+import { BinanceRepository } from './repositories'
+import { BitfinexRepository } from './repositories'
+import { BitgetRepository } from './repositories'
+import { BitstampRepository } from './repositories'
+import { CoinbaseRepository } from './repositories'
+import { GateIORepository } from './repositories'
+import { KrakenRepository } from './repositories'
+import { KuCoinRepository } from './repositories'
+import { MercadoBitcoinRepository } from './repositories'
+import { OKXRepository } from './repositories'
 
 describe('TickerService', () => {
   let service: TickerService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TickerService, BinanceRepository],
+      providers: [
+        TickerService,
+        BinanceRepository,
+        BitfinexRepository,
+        BitgetRepository,
+        BitstampRepository,
+        CoinbaseRepository,
+        GateIORepository,
+        KrakenRepository,
+        KuCoinRepository,
+        MercadoBitcoinRepository,
+        OKXRepository,
+      ],
       imports: [HttpModule],
     }).compile()
 
--- src/domain/ticker/ticker.service.ts
+++ src/domain/ticker/ticker.service.ts
@@ -1,14 +1,59 @@
 import { Injectable } from '@nestjs/common'
-import { BinanceRepository } from './repositories/binance.repository'
 import { TickerRequestDto, TickerResponseDto } from './dto'
+
+import { BinanceRepository } from './repositories'
+import { BitfinexRepository } from './repositories'
+import { BitgetRepository } from './repositories'
+import { BitstampRepository } from './repositories'
+import { CoinbaseRepository } from './repositories'
+import { GateIORepository } from './repositories'
+import { KrakenRepository } from './repositories'
+import { KuCoinRepository } from './repositories'
+import { MercadoBitcoinRepository } from './repositories'
+import { OKXRepository } from './repositories'
 
 @Injectable()
 export class TickerService {
-  constructor(private readonly repository: BinanceRepository) {}
+  repositories: Array<any>
+
+  constructor(
+    private readonly binanceRepository: BinanceRepository,
+    private readonly bitfinexRepository: BitfinexRepository,
+    private readonly bitgetRepository: BitgetRepository,
+    private readonly bitstampRepository: BitstampRepository,
+    private readonly coinbaseRepository: CoinbaseRepository,
+    private readonly gateioRepository: GateIORepository,
+    private readonly krakenRepository: KrakenRepository,
+    private readonly kucoinRepository: KuCoinRepository,
+    private readonly mercadoBitcoinRepository: MercadoBitcoinRepository,
+    private readonly okxRepository: OKXRepository,
+  ) {
+    this.repositories = [
+      this.binanceRepository,
+      this.bitgetRepository,
+      this.coinbaseRepository,
+      this.kucoinRepository,
+
+      // No BRL data
+      this.bitfinexRepository,
+      this.bitstampRepository,
+      this.gateioRepository,
+      this.krakenRepository,
+      this.okxRepository,
+
+      // No BRL data
+      this.mercadoBitcoinRepository,
+    ]
+  }
 
   async getTicker(params: TickerRequestDto): Promise<TickerResponseDto> {
-    const data = await this.repository.getTicker(params)
-    if (data) return data
+    for (let repository of this.repositories) {
+      try {
+        const data = await repository.getTicker(params)
+        if (data) return data
+      } catch {}
+    }
+
     throw new Error()
   }
 }
