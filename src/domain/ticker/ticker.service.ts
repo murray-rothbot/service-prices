245cb1f4c8fd3dc61c32c81570e355d9671fd6e5
vforvilela <henrique@vilela.one>
2022-12-20 09:36:14 -0300

    feat(cache): add cache capabilities to the tickers module

--- src/domain/ticker/ticker.service.ts
+++ src/domain/ticker/ticker.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common'
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common'
+import { Cache } from 'cache-manager'
+import { Cron, CronExpression } from '@nestjs/schedule'
 import { TickerRequestDto, TickerResponseDto, TickersResponseDto } from './dto'
 import { ITickerRepository } from './interfaces'
+
 import {
   BinanceRepository,
   BitfinexRepository,
@@ -19,16 +22,17 @@ export class TickerService {
   repositories: Array<ITickerRepository>
 
   constructor(
-    private readonly binanceRepository: BinanceRepository,
-    private readonly bitfinexRepository: BitfinexRepository,
-    private readonly bitgetRepository: BitgetRepository,
-    private readonly bitstampRepository: BitstampRepository,
-    private readonly coinbaseRepository: CoinbaseRepository,
-    private readonly gateioRepository: GateIORepository,
-    private readonly krakenRepository: KrakenRepository,
-    private readonly kucoinRepository: KuCoinRepository,
-    private readonly mercadoBitcoinRepository: MercadoBitcoinRepository,
-    private readonly okxRepository: OKXRepository,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+    private readonly binanceRepository?: BinanceRepository,
+    private readonly bitfinexRepository?: BitfinexRepository,
+    private readonly bitgetRepository?: BitgetRepository,
+    private readonly bitstampRepository?: BitstampRepository,
+    private readonly coinbaseRepository?: CoinbaseRepository,
+    private readonly gateioRepository?: GateIORepository,
+    private readonly krakenRepository?: KrakenRepository,
+    private readonly kucoinRepository?: KuCoinRepository,
+    private readonly mercadoBitcoinRepository?: MercadoBitcoinRepository,
+    private readonly okxRepository?: OKXRepository,
   ) {
     this.repositories = [
       this.binanceRepository,
@@ -44,13 +48,16 @@ export class TickerService {
 
       this.krakenRepository, // No BRL data, no 24 change data
       this.mercadoBitcoinRepository, // No USD data, no 24 change data
-    ]
+    ].filter((repo) => repo != null)
   }
 
-  async getTicker(params: TickerRequestDto): Promise<TickerResponseDto> {
+  async getTicker({ symbol }: TickerRequestDto): Promise<TickerResponseDto> {
     for (let repository of this.repositories) {
       try {
-        const data = await repository.getTicker(params)
+        const key = `${repository.source}_${symbol}`
+        let data: TickerResponseDto = await this.cacheManager.get(key)
+        if (!data) data = await repository.getTicker({ symbol })
+
         if (data) return data
       } catch {}
     }
@@ -58,15 +65,36 @@ export class TickerService {
     throw new Error()
   }
 
-  async getTickers(params: TickerRequestDto): Promise<TickersResponseDto> {
+  async getTickers({ symbol }: TickerRequestDto): Promise<TickersResponseDto> {
     const promises = this.repositories.map(
       async (repository: ITickerRepository): Promise<TickerResponseDto> => {
-        return await repository.getTicker(params)
+        let data: TickerResponseDto = await this.cacheManager.get(`${repository.source}_${symbol}`)
+        if (!data) data = await repository.getTicker({ symbol })
+        return data
       },
     )
 
     const tickers = (await Promise.all(promises)).filter((ticker) => ticker != null)
 
     return { tickers }
+  }
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async updateCache() {
+    const symbols = ['btcusd', 'btcbrl']
+
+    for (const symbol of symbols) {
+      const promises = this.repositories.map(
+        async (repository: ITickerRepository): Promise<TickerResponseDto> => {
+          return await repository.getTicker({ symbol })
+        },
+      )
+
+      const tickers = (await Promise.all(promises)).filter((ticker) => ticker != null)
+      for (const ticker of tickers) {
+        const key = `${ticker.source}_${symbol}`
+        await this.cacheManager.set(key, ticker, 0)
+      }
+    }
   }
 }
